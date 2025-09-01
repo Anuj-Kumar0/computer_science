52ccfd835b768d0b52adf0b1ec64a38769f97109
Anuj Kumar <anujkumar21052004@gmail.com>
2025-09-01 14:22:02 +0530

    set method

--- HashMap/hashmap.js
+++ HashMap/hashmap.js
@@ -1,6 +1,9 @@
-const hashMap = function () {
-  let loadFactor;
-  let capacity;
+export const hashMap = function () {
+  let loadFactor = 0.75;
+  let capacity = 16;
+  let growth = loadFactor * capacity;
+  let bucket = new Array(capacity).fill().map(() => []);
+
   return {
     hash(key) {
       let hashCode = 0;
@@ -8,13 +11,26 @@ const hashMap = function () {
       const primeNumber = 31;
       for (let i = 0; i < key.length; i++) {
         hashCode = primeNumber * hashCode + key.charCodeAt(i);
-        hashCode %= 16;
+        hashCode %= capacity;
       }
 
       return hashCode;
     },
     set(key, value) {
       let index = this.hash(key);
+      for (let key_value of bucket[index]) {
+        if (key_value[0] === key) {
+          key_value[1] = value;
+          console.log(bucket);
+          return;
+        }
+      }
+      bucket[index].push([key, value]);
+      console.log(bucket);
+
+      if (bucket[index] > growth) {
+        capacity *= 2;
+      }
     },
   };
 };
