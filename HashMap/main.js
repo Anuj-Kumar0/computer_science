39bffb8b6417817bce23c54b9e6c1c45317ff32a
Anuj Kumar <anujkumar21052004@gmail.com>
2025-09-02 17:09:23 +0530

    Hashmap growth, get, has testing

--- HashMap/main.js
+++ HashMap/main.js
@@ -3,3 +3,12 @@ import { hashMap } from "./hashmap.js";
 const map = hashMap();
 map.set("carlos", "i am the old value");
 map.set("carlos", "i am the new value");
+
+// for (let i = 1; i <= 20; i++) {
+//   map.set(`key${i}`, `value${i}`);
+//   console.log(`Inserted key${i}, capacity:`, map.capacity);
+//   console.log(map.debug());
+// }
+
+// console.log(map.get("carlos"));
+// console.log(map.has("carlo"));
