fb96f96851395be2f0b6aa5cbe7593155af72602
Anuj Kumar <anujkumar21052004@gmail.com>
2025-09-02 17:02:49 +0530

    Hashmap growth, has, get methods

--- HashMap/hashmap.js
+++ HashMap/hashmap.js
@@ -3,7 +3,22 @@ export const hashMap = function () {
   let capacity = 16;
   let growth = loadFactor * capacity;
   let bucket = new Array(capacity).fill().map(() => []);
+  let size = 0;
 
+  function doubleSize(hashFunc) {
+    capacity *= 2;
+    growth = loadFactor * capacity;
+
+    let newBucket = new Array(capacity).fill().map(() => []);
+
+    for (let a of bucket) {
+      for (let [b, c] of a) {
+        let index = hashFunc(b);
+        newBucket[index].push([b, c]);
+      }
+    }
+    bucket = newBucket;
+  }
   return {
     hash(key) {
       let hashCode = 0;
@@ -26,11 +41,32 @@ export const hashMap = function () {
         }
       }
       bucket[index].push([key, value]);
+      size++;
       console.log(bucket);
 
-      if (bucket[index] > growth) {
-        capacity *= 2;
+      if (size > growth) {
+        doubleSize(this.hash);
       }
+    },
+    get(key) {
+      let index = this.hash(key);
+      for (let key_value of bucket[index]) {
+        if (key_value[0] === key) {
+          return key_value[1];
+        }
+      }
+    },
+    has(key) {
+      let index = this.hash(key);
+      for (let key_value of bucket[index]) {
+        if (key_value[0] === key) {
+          return true;
+        }
+      }
+      return false;
+    },
+    debug() {
+      return { capacity, size, growth, bucket };
     },
   };
 };
