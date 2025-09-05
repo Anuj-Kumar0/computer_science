acc612276117861c37a40956ee0e54cefd4f2fd2
Anuj Kumar <anujkumar21052004@gmail.com>
2025-09-05 12:58:55 +0530

    Balanced BST

---
+++ BinarySearchTrees/bst.js
@@ -0,0 +1,52 @@
+const node = function (data, left = null, right = null) {
+  return {
+    data,
+    left,
+    right,
+  };
+};
+
+const tree = function (array) {
+  return {
+    root: buildTree(array),
+  };
+};
+
+function buildTree(array) {
+  const sorted = array.sort((a, b) => a - b);
+  const uniqueArray = [...new Set(sorted)];
+
+  function sortedArrayToBST(uniqueArray, start, end) {
+    if (start > end) {
+      return null;
+    }
+
+    let mid = Math.floor((start + end) / 2);
+
+    let root = new node(uniqueArray[mid]);
+
+    root.left = sortedArrayToBST(uniqueArray, start, mid - 1);
+    root.right = sortedArrayToBST(uniqueArray, mid + 1, end);
+
+    return root;
+  }
+  return sortedArrayToBST(uniqueArray, 0, uniqueArray.length - 1);
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+let array = [1, 5, 6, 8, 2, 3, 4, 7, 9];
+
+const root = buildTree(array);
+prettyPrint(root);
